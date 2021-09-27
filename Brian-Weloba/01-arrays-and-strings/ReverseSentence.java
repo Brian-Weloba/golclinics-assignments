696c77ecdf9b93560f04e5a04a1efb8b7cdc2b8c
Brian-Weloba <bweloba@gmail.com>
2021-09-27 17:34:14 +0300

    Second challenge

--- Brian-Weloba/01-arrays-and-strings/ReverseSentence.java
+++ Brian-Weloba/01-arrays-and-strings/ReverseSentence.java
@@ -1,17 +1,25 @@
+import java.util.Arrays;
+
 class ReverseSentence {
     public static void main(String[] args) {
         char[] A = { 't', 'h', 'i', 's', ' ', 'i', 's', ' ', 'g', 'o', 'o', 'd' };
-        reverseSentence(A);// ['g','o','o','d',' ','i','s',' ','t','h','i','s']
+        System.out.println(Arrays.toString(reverseSentence(A))); //['g','o','o','d',' ','i','s',' ','t','h','i','s']
     }
 
-    public static String[] reverseSentence(char[] a) {
+    public static char[] reverseSentence(char[] a) {
+        //change input to string
         String sentence = String.valueOf(a);
+        //split string at spaces and make string array
         String[] words = sentence.split(" ");
+        //reverse the words in the string array
         for (int i = 0; i < words.length / 2; i++) {
             String temp = words[i];
             words[i] = words[words.length - 1 - i];
             words[words.length - 1 - i] = temp;
         }
-
+        //join string[] to one string
+        String joined = String.join(" ", words);
+        //split back to chararray
+        return joined.toCharArray();
     }
 }
